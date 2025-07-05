4e71a1ec2479e756c2ecb4789203bb56331d0f61
Mikyac48 <miguel.ardite@estudiantat.upc.edu>
2025-07-06 00:40:29 +0200

    Codigo con 3 textos post-it

--- Assets/toDoList.cs
+++ Assets/toDoList.cs
@@ -3,38 +3,56 @@ using TMPro;
 
 public class toDoList : MonoBehaviour
 {
-    public TextMeshProUGUI tmpText;
+    public TextMeshProUGUI tmpText1;
+    public TextMeshProUGUI tmpText2;
+    public TextMeshProUGUI tmpText3;
 
     public float duration = 30f;
 
     public float maxDilate = 1f;
 
-    private static readonly int ID_FaceDilate = Shader.PropertyToID("_FaceDilate");
-    private float elapsed = 0f;
+    private float elapsed1 = 0f;
+    private float elapsed2 = 0f;
+    private float elapsed3 = 0f;
+
     private Material matInstance;
 
     void Awake()
     {
-        matInstance = Instantiate(tmpText.fontMaterial);
-        tmpText.fontMaterial = matInstance;
+        //matInstance = Instantiate(tmpText.fontMaterial);
+        //tmpText.fontMaterial = matInstance;
     }
 
     void Update()
     {
-        elapsed += Time.deltaTime;
-        float t = Mathf.Clamp01(elapsed / duration);
+        elapsed1 += Time.deltaTime;
+        elapsed2 += Time.deltaTime;
+        elapsed3 += Time.deltaTime;
 
-        float dilate = Mathf.Lerp(0f, maxDilate, t);
-        matInstance.SetFloat(ID_FaceDilate, dilate);
+        float t1 = Mathf.Clamp01(elapsed1 / duration);
+        float t2 = Mathf.Clamp01(elapsed2 / duration);
+        float t3 = Mathf.Clamp01(elapsed3 / duration);
 
-        Color c = tmpText.color;
-        c.a = 1f - t;
-        tmpText.color = c;
 
-        if (elapsed >= duration)
+        Color c = tmpText1.color;
+        c.a = 1f - t1;
+        tmpText1.color = c;
+        c.a = 1f - t2;
+        tmpText2.color = c;
+        c.a = 1f - t3;
+        tmpText3.color = c;
+
+        if (elapsed1 >= duration)
         {
-            tmpText.text = "";
-            enabled = false;
+            tmpText1.text = "";
+        }
+        if (elapsed2 >= duration)
+        {
+            tmpText2.text = "";
+        }
+        if (elapsed3 >= duration)
+        {
+            tmpText3.text = "";
         }
     }
 }
