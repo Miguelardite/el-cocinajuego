632c09ceb78fc6529b8296a1dad1cb1f81e8ca39
Mikyac48 <miguel.ardite@estudiantat.upc.edu>
2025-07-06 01:31:17 +0200

    Conexion ventilador con post-it

--- Assets/toDoList.cs
+++ Assets/toDoList.cs
@@ -14,13 +14,46 @@ public class toDoList : MonoBehaviour
     private float elapsed1 = 0f;
     private float elapsed2 = 0f;
     private float elapsed3 = 0f;
-
-    private Material matInstance;
+    
+    private bool enabled1, enabled2, enabled3;
 
     void Awake()
     {
-        //matInstance = Instantiate(tmpText.fontMaterial);
-        //tmpText.fontMaterial = matInstance;
+        enabled1 = false;
+        enabled2 = false;
+        enabled3 = false;
+    }
+
+    void inicia(int pos, string text)
+    {
+        switch (pos)
+        {
+            case 1:
+                enabled1 = false;
+                elapsed1 = 0f;
+                tmpText1.text = text;       //iniciar tareas nuevas
+                break;
+            case 2:                         //1 = top, 2 = mid, 3 = bot
+                enabled2 = false;
+                elapsed2 = 0f;
+                tmpText2.text = text;
+                break;
+            case 3:
+                enabled3 = false;
+                elapsed3 = 0f;
+                tmpText3.text = text;
+                break;
+        }
+    }
+
+    public void iniciaTarea(string text)
+    {
+        if (enabled1) inicia(1, text);        //toda esta parrafada es para ver donde poner la tarea nueva
+        else if (enabled2) inicia(2, text);   //si hay un espacio libre la pone ahi, si no, sustituye la tarea mas antigua
+        else if (enabled3) inicia(3, text);
+        else if (elapsed1 > elapsed2 && elapsed1 > elapsed3) inicia(1, text);
+        else if (elapsed2 > elapsed1 && elapsed2 > elapsed3) inicia(2, text);
+        else inicia(3, text);
     }
 
     void Update()
@@ -45,14 +78,17 @@ public class toDoList : MonoBehaviour
         if (elapsed1 >= duration)
         {
             tmpText1.text = "";
+            enabled1 = true;
         }
         if (elapsed2 >= duration)
         {
             tmpText2.text = "";
+            enabled2 = true;
         }
         if (elapsed3 >= duration)
         {
             tmpText3.text = "";
+            enabled3 = true;
         }
     }
 }
