02e8645d94021d31b63e21a26ba1b6ba6e5e13f5
Mikyac48 <miguel.ardite@estudiantat.upc.edu>
2025-07-07 16:55:21 +0200

    pitido nevera

--- Assets/Scripts/Props/Fridge.cs
+++ Assets/Scripts/Props/Fridge.cs
@@ -4,10 +4,16 @@ using static UnityEngine.RuleTile.TilingRuleOutput;
 public class Fridge : PropGeneric
 {
     bool freezeChicken;
+    private bool lastActive = false;
+    private bool playing;
+    public float openTime;
+    public AudioSource pitido;
     public void Awake()
     {
         isActive = false; // Activo = abierto e inactivo = cerrado
         freezeChicken = true;
+        playing = false;
+        openTime = 0f;
     }
     public override void Action()
     {
@@ -22,6 +28,23 @@ public class Fridge : PropGeneric
     }
     private void Update()
     {
+        if (isActive)
+        {
+            if (!lastActive) openTime = 0f;
+            openTime += Time.deltaTime;
+            if (openTime >= 20f && !playing)
+            {
+                pitido.Play();
+                playing = true;
+            }
+        }
+        else
+        {
+            playing = false;
+            pitido.Stop();
+        }
+        lastActive = isActive;
+
         if (freezeChicken && !isActive && chicken.GetComponent<Chicken>().frozenPercent < 50)
         {
             chicken.GetComponent<Chicken>().frozenPercent += Time.deltaTime;
