8d718216291555b4336b5afd24451395dfc5bdfe
KuroG3 <janespanamiras@gmail.com>
2025-07-09 17:37:55 +0200

    Escena inicial hecha falta decorar

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using TMPro;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartScreen : MonoBehaviour
+{
+    public TextMeshProUGUI spaceText;
+    public float blinkSpeed = 1.5f;
+    private float alpha = 1f;
+    private bool fadingOut = true;
+
+    public string scene;
+
+    void Update()
+    {
+        Blink();
+        // Check for space key press to start the game
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartGame();
+        }
+    }
+    void Blink()
+    {
+        float fadeAmount = Time.deltaTime / (blinkSpeed / 2f);
+
+        Color currentColor = spaceText.color;
+
+        if (fadingOut)
+        {
+            alpha -= fadeAmount;
+            if (alpha <= 0f)
+            {
+                alpha = 0f;
+                fadingOut = false;
+            }
+        }
+        else
+        {
+            alpha += fadeAmount;
+            if (alpha >= 1f)
+            {
+                alpha = 1f;
+                fadingOut = true;
+            }
+        }
+
+        currentColor.a = alpha;
+        spaceText.color = currentColor;
+    }
+    void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+    }
+}
